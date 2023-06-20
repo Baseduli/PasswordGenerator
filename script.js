cb977f0e94ceb3655dc738b6457784c7f0a5ad0b
Ulises <ulisesvivar123@gmail.com>
2023-06-19 21:47:13 -0400

    worked on the script trying to figure out what steps come next. Able to see how the password returns. Need to figure out a way so the function generatePassword knows how to validate the responses inside the window.prompt

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-window.alert("Let's create a password for you");
-var passwordLength = window.prompt("Pick password length");
-var specialCharacters = window.confirm("Special Characters?");
-var upperCase = window.confirm("Uppercase letters?");
-var lowerCase = window.confirm("Lowercase letters?");
-
-//console.log(passwordLength);
-//console.log(specialCharacters);
-//console.log(upperCase);
-//console.log(lowerCase);
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword(passwordLength, specialCharacters, upperCase, lowerCase) {
+  console.log("Click");
+  var passwordLength = window.prompt("Pick password length between 8 and 128");
+  var specialCharacters = window.confirm("Special Characters?");
+  var upperCase = window.confirm("Uppercase letters?");
+  var lowerCase = window.confirm("Lowercase letters?");
+
+  return "Generated password here";
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
